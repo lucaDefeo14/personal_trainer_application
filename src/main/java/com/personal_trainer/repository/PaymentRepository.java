523df6a31ca65f870655bd45325a00a15f8d6323
Luca De Feo <luca.defeo@finconsgroup.com>
2025-09-15 12:22:31 +0200

    aggiornamenti

--- src/main/java/com/personal_trainer/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.personal_trainer.repository;
-
-import com.personal_trainer.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment,Long> {
-}
--- src/main/java/com/personal_trainer/repository/TrainingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.personal_trainer.repository;
-
-import com.personal_trainer.entity.Training;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainingRepository extends JpaRepository<Training,Long> {
-}
